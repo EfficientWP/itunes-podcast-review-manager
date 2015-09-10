e1dbf674e6d99aa3370678f39178d209511e247c
Ryan Novotny <ryan.novotny@gmail.com>
2015-09-10 11:41:27 -0700

    use options table instead of user meta -> allows shortcodes to be seen by visitors

--- includes/data-processing-functions.php
+++ includes/data-processing-functions.php
@@ -20,19 +20,11 @@ function iprm_deactivate() {
 }
 
 function iprm_delete_option( $meta_key ) {
-	$user_ID = get_current_user_id();
-	return delete_user_meta( $user_ID, $meta_key );
-
+	return delete_option( $meta_key );
 }
 function iprm_get_option( $option ) {
-	$user_ID = get_current_user_id();
-	return get_user_meta( $user_ID, $option, TRUE );
-
+	return get_option( $option, FALSE );
 }
 function iprm_update_option( $meta_key, $meta_value ) {
-
-	$user_ID = get_current_user_id();
-	return update_user_meta( $user_ID, $meta_key, $meta_value );
-
-
+	return update_option( $meta_key, $meta_value );
 }
